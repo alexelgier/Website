626330b3bc4ea9dd4e45a21417cdff0667ece6ae
alexelgier <alexander.elgier@gmail.com>
2016-08-29 00:24:20 -0300

    Working.

--- website/src/main/java/com/alexelgier/website/WebAppInitializer.java
+++ website/src/main/java/com/alexelgier/website/WebAppInitializer.java
@@ -24,8 +24,7 @@ public class WebAppInitializer implements WebApplicationInitializer {
 
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/*");
-
+        dispatcher.addMapping("/");
         servletContext.addListener(new ContextLoaderListener(appContext));
 
         //Database Console for managing the app's database (TODO : profile)
